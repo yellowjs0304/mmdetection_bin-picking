23832ba4c4c0cdd405343207d6b2d6b97c022f79
Jisu.H <30281608+yellowjs0304@users.noreply.github.com>
2022-09-22 10:51:18 +0900

    Update coco_instance.py

--- configs/_base_/datasets/coco_instance.py
+++ configs/_base_/datasets/coco_instance.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/home/jovyan/local/1_user/jshwang@agilesoda.ai/mmdetection/data/coco/'
+data_root = '[DATA_ROOT]'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
